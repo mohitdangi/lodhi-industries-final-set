48f0d870a3c21299e6a722d193eaeeae5b92c72a
Your Name <mohitdangi58368@gmail.com>
2023-09-12 17:12:03 +0530

    mohit bhai chutiye7

--- script.js
+++ script.js
@@ -36,33 +36,34 @@ valueDisplays.forEach((valueDisplay) => {
 });
 //  toast
 // Get the modal element
-var modal = document.getElementById("myModal");
+      var modal = document.getElementById("myModal");
 
-// Get the button that opens the modal
-var sendButton = document.getElementById("sendButton");
+        // Get the button that opens the modal
+        var sendButton = document.getElementById("sendButton");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
 
-// Get the email input field
-var emailInput = document.getElementById("emailInput");
+        // Get the email input field
+        var emailInput = document.getElementById("emailInput");
 
-// When the user clicks the button, check if email is provided and open the modal
-sendButton.onclick = function () {
-    if (emailInput.checkValidity()) {
-        modal.style.display = "block";
-    }
-    
-}
+        // When the user clicks the button, check if email is provided and open the modal
+        sendButton.onclick = function (event) {
+          if (emailInput.checkValidity()) {
+              modal.style.display = "block";
+          }
+          event.preventDefault(); // Prevent the default form submission behavior
+      }
+      
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function () {
+            modal.style.display = "none";
+        }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
