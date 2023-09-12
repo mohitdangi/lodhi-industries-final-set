c35268768a300f4f64799ad24f5a2c67e636fc4f
Your Name <mohitdangi58368@gmail.com>
2023-09-12 16:03:14 +0530

    mohit bhai chutiye5

--- script.js
+++ script.js
@@ -35,3 +35,33 @@ valueDisplays.forEach((valueDisplay) => {
   }, duration);
 });
 //  toast
+// Get the modal element
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var sendButton = document.getElementById("sendButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Get the email input field
+var emailInput = document.getElementById("emailInput");
+
+// When the user clicks the button, check if email is provided and open the modal
+sendButton.onclick = function () {
+    if (emailInput.checkValidity()) {
+        modal.style.display = "block";
+    }
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
